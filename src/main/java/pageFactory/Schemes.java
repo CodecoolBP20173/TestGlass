67e074a7fb9b69103d29371eca2e5b84e1c9eec3
Szilárd <bszilard91@gmail.com>
2019-04-02 11:10:16 +0200

    Navigation to the scheme page

--- src/main/java/pageFactory/Schemes.java
+++ src/main/java/pageFactory/Schemes.java
@@ -1,4 +1,30 @@
 package pageFactory;
 
-public class Schemes {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class Schemes extends PageObject{
+
+    @FindBy(id = "aui-uid-3")
+    WebElement schemeButton;
+
+    @FindBy(xpath = "//*[@id='glass-general-schemes-panel']//span[text()='Schemes ']")
+    WebElement schemeTitle;
+
+    @FindBy(xpath = "//*[@id='glass-general-schemes-panel']//table[@class='aui aui-table-sortable']//a[contains(@href,'listDetails.do')]")
+    WebElement table;
+
+    public Schemes(WebDriver driver) {
+        super(driver);
+    }
+
+    public void openSchemes() {
+        schemeButton.click();
+    }
+
+    public String getSchemeTitle() {
+        return schemeTitle.getText();
+    }
+
 }
--- src/main/java/test/SchemesTest.java
+++ src/main/java/test/SchemesTest.java
@@ -1,4 +1,40 @@
 package test;
 
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.WebDriver;
+import pageFactory.Login;
+import pageFactory.Schemes;
+import util.RunEnvironment;
+import util.Utils;
+
 public class SchemesTest {
+
+    Login login;
+    Schemes schemes;
+    WebDriver driver;
+
+    @BeforeEach
+    public void setup() {
+        Utils.setupFromNode();
+        driver = RunEnvironment.getWebDriver();
+        login = new Login(driver);
+        schemes = new Schemes(driver);
+        driver.manage().window().setSize(new Dimension(1280, 720));
+    }
+
+    @DisplayName("This a test test")
+    @Test
+    void schemeNavigationTest() {
+        login.login();
+        driver.get("https://jira2.codecool.codecanvas.hu/projects/SE?selectedItem=com.codecanvas.glass:glass");
+        schemes.openSchemes();
+        Assertions.assertEquals("Schemes", schemes.getSchemeTitle(), "Navigation schemes");
+    }
+
+    @AfterEach
+    public void tearDown() {
+        Utils.tearDown();
+    }
+
 }
