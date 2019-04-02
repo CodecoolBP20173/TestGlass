5d33143446ebe92e3ab27899da6b2ff6c7072339
Szilárd <bszilard91@gmail.com>
2019-04-02 08:31:33 +0200

    Added Login to pageFactroy and a simple login test.

--- src/main/java/pageFactory/Login.java
+++ src/main/java/pageFactory/Login.java
@@ -1,4 +1,62 @@
 package pageFactory;
 
-public class Login {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class Login extends PageObject {
+
+    @FindBy(id = "login-form-username")
+    WebElement userName;
+
+    @FindBy(id = "login-form-password")
+    WebElement password;
+
+    @FindBy(id = "login-form-submit")
+    WebElement loginBtn;
+
+    @FindBy(id = "user-options")
+    WebElement userOptions;
+
+    @FindBy(id = "log_out")
+    WebElement logoutBtn;
+
+    @FindBy(xpath = "//*[@id='login-form']//div[@class='aui-message error']")
+    WebElement errorMessage;
+
+    @FindBy(id = "data-displayname")
+    WebElement logCheck;
+
+    @FindBy(xpath = "//*[@id='content']//h1[text(), 'Logout']")
+    WebElement logoutSign;
+
+    public Login(WebDriver driver) {
+        super(driver);
+    }
+
+    public void login() {
+        driver.navigate().to("https://jira2.codecool.codecanvas.hu/login.jsp");
+        userName.sendKeys(System.getenv("username"));
+        password.sendKeys(System.getenv("password"));
+        loginBtn.click();
+    }
+
+    public boolean isLoggedIn() {
+        return !userOptions.getText().equals("Log In");
+    }
+
+    public void logout() {
+        userOptions.click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("log_out")));
+        logoutBtn.click();
+    }
+
+    public boolean wrongLogin() {
+        return errorMessage.isDisplayed();
+    }
+
 }
--- src/main/java/test/LoginTest.java
+++ src/main/java/test/LoginTest.java
@@ -1,4 +1,37 @@
 package test;
 
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.WebDriver;
+import pageFactory.Login;
+import util.RunEnvironment;
+import util.Utils;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class LoginTest {
+
+    Login login;
+    WebDriver driver;
+
+    @BeforeEach
+    public void setup() {
+        Utils.setupFromNode();
+        driver = RunEnvironment.getWebDriver();
+        login = new Login(driver);
+        driver.manage().window().setSize(new Dimension(1280, 720));
+    }
+
+    @Test
+    void testLogin() {
+        login.login();
+        Assertions.assertTrue(login.isLoggedIn());
+    }
+
+    @AfterEach
+    public void tearDown() {
+        Utils.tearDown();
+    }
 }
